04f24355124bc2cfa04e2e6f1215bbdf8ac38621
Yiranubari <yiranubari4@gmail.com>
2026-04-18 16:46:25 +0100

    feat: initialize shared type definitions and update server build paths for path aliasing

---
+++ shared/types/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Priority = exports.TaskStatus = void 0;
+// Tasks
+var task_types_1 = require("./task.types");
+Object.defineProperty(exports, "TaskStatus", { enumerable: true, get: function () { return task_types_1.TaskStatus; } });
+Object.defineProperty(exports, "Priority", { enumerable: true, get: function () { return task_types_1.Priority; } });
